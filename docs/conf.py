d038bd832699ef1af5df9fb23dce2eb4e67c43ca
Jonathan Karr <jonrkarr@gmail.com>
2016-08-18 16:35:38 -0400

    removing encode version

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ author = u'Jonathan Karr'
 # built documents.
 #
 # The short X.Y version.
-version = nose2unitth.__version__.encode('utf-8')
+version = nose2unitth.__version__
 # The full version, including alpha/beta/rc tags.
 release = version
 
